1b422e715626cc1c82ebefdbf206af4f1c7986a6
Ahmed Dogan <114875634+MuhammedAhmedDogan@users.noreply.github.com>
2024-09-01 18:37:52 +0300

    Note App

--- React/Note App/src/App.jsx
+++ React/Note App/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TextArea from './components/textArea/TextArea';
 import Buttons from './components/buttons/Buttons';
 import SavedNotes from './components/savedNotes/SavedNotes';
@@ -7,36 +7,82 @@ import './App.css';
 function App() {
   const [note, setNote] = useState('');
   const [notes, setNotes] = useState([]);
+  const [filteredNotes, setFilteredNotes] = useState([]);
   const [search, setSearch] = useState('');
+  const [selectedId, setSelectedId] = useState(0);
   const [selectedColor, setSelectedColor] = useState('red');
   const [showAlert, setShowAlert] = useState(false);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
+  const [showUpdatedMessage, setShowUpdatedMessage] = useState(false);
+  const [showDeletedMessage, setShowDeletedMessage] = useState(false);
 
   const saveNote = () => {
     setShowAlert(false);
-    if (note.trim() !== '') {
-      const newNote = { id: notes.length + 1, note: note, color: selectedColor };
-      setNotes(prevNotes => [...prevNotes, newNote]);
-      setNote('');
+    if (selectedId === 0) {
+      if (note.trim() !== '') {
+        const maxId = notes.length ? notes.reduce((max, item) => item.id > max ? item.id : max, notes[0].id) : 0;
+        const newNote = { id: maxId + 1, note: note, color: selectedColor };
+        setNotes(prevNotes => [...prevNotes, newNote]);
+        setNote('');
+        setShowAddedMessage(true);
+        setTimeout(() => {
+          setShowAddedMessage(false);
+        }, 2000);
+      } else {
+        setShowAlert(true);
+        setTimeout(() => {
+          setShowAlert(false);
+        }, 2000);
+      }
     } else {
-      setShowAlert(true);
-      setTimeout(() => {
-        setShowAlert(false);
-      }, 2000);
+      if (note.trim() === '') {
+        setNotes(prevNotes => prevNotes.filter(item => item.id !== selectedId));
+        setNote('');
+        setSelectedId(0);
+        setShowDeletedMessage(true);
+        setTimeout(() => {
+          setShowDeletedMessage(false);
+        }, 2000);
+      } else {
+        setNotes(prevNotes => prevNotes.map(item => item.id === selectedId ? { ...item, note: note, color: selectedColor } : item));
+        setNote('');
+        setSelectedId(0);
+        setShowUpdatedMessage(true);
+        setTimeout(() => {
+          setShowUpdatedMessage(false);
+        }, 2000);
+      }
     }
   };
+
+
+  useEffect(() => {
+    setFilteredNotes(notes.filter(item => item.note.includes(search)));
+  }, [search, notes])
+
+  useEffect(() => {
+    if (selectedId !== 0) {
+      const selectedNote = notes.find(item => item.id === selectedId);
+      setNote(selectedNote.note);
+      setSelectedColor(selectedNote.color);
+    }
+  }, [selectedId, notes])
 
   return (
     <>
       <div className='container-top'>
         <h1 className='title'>NotesApp</h1>
-        <input value={search} onChange={(e) => (setSearch(e.target.value))} type="text" name="search" id="search" placeholder='Search...' />
+        <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" name="search" id="search" placeholder='Search...' />
         <TextArea note={note} setNote={setNote} />
         <Buttons selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
-        <button className='add-btn' onClick={saveNote}>ADD</button>
-        <h3 className={`alert ${showAlert ? 'show' : ''}`}>Please fill out the note field to save your note.</h3>
+        <button className='add-btn' onClick={saveNote}>{selectedId === 0 ? 'ADD' : (note.trim() === '' ? 'DELETE' : 'SAVE')}</button>
+        <h3 className={`alert ${showAlert ? 'show' : ''}`}>Please fill out the note field to save your note!</h3>
+        <h3 className={`alert ${showAddedMessage ? 'show' : ''}`}>Note added successfully!</h3>
+        <h3 className={`alert ${showUpdatedMessage ? 'show' : ''}`}>Note updated successfully!</h3>
+        <h3 className={`alert ${showDeletedMessage ? 'show' : ''}`}>Note deleted!</h3>
       </div>
       <div className='container-bottom'>
-        <SavedNotes notes={notes} />
+        <SavedNotes notes={search ? filteredNotes : notes} setSelectedId={setSelectedId} />
       </div>
     </>
   )
--- React/Note App/src/components/savedNotes/SavedNotes.jsx
+++ React/Note App/src/components/savedNotes/SavedNotes.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
 import './SavedNotes.css'
 
-function SavedNotes({ notes }) {
+function SavedNotes({ notes, setSelectedId }) {
+
+  function handleClickNote(e) {
+    setSelectedId(Number(e.currentTarget.id));
+  }
+  
   return (
     <div className='note-cards-container'>
       {notes.map(item => (
-        <div key={item.id} className={`note-card ${item.color}-card`}>
+        <div key={item.id} id={item.id} className={`note-card ${item.color}-card`} onClick={(e) => handleClickNote(e)}>
           <h4>{item.note}</h4>
         </div>
       ))}
@@ -15,6 +20,7 @@ function SavedNotes({ notes }) {
 
 SavedNotes.propTypes = {
   notes: PropTypes.array.isRequired,
+  setSelectedId: PropTypes.func.isRequired
 };
 
 export default SavedNotes
